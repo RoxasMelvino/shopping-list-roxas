f60bf325c4da87012bbb8161d5e39e84bb4548ea
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2024-08-09 10:27:16 -0700

    add dark mode

--- script.js
+++ script.js
@@ -2,17 +2,10 @@ const addItemButton = document.querySelector('#addItemButton');
 const items = document.querySelector('ul#items');
 const form = document.querySelector('form');
 const clearButton = document.querySelector('#clearButton');
+const cartShopping = document.querySelector('.fa-cart-shopping')
+const container = document.querySelector('.container');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const item = document.querySelector('#inputItem').value;
-    addItem(item);
-});
-
-clearButton.addEventListener('click', clearAll)
-
-
+// functions ---
 function addItem(item) {
     const newItem = document.createElement('li');
     const text = document.createTextNode(item);
@@ -31,15 +24,41 @@ function clearAll() {
     } else {
         alert('List is empty.');
     }
+};
+
+function doubleClick() {
+    if (document.body.style.backgroundColor !== "#0F141A") {
+        document.body.style.backgroundColor = "#0F141A";
+        document.body.style.color = "#fff";
+        container.style.border = "1px solid #fff";
+    } else {
+        document.body.style.backgroundColor = "#fff";
+        document.body.style.color = "#000";
+        container.style.border = "none";
+    }
+};
+
+function dragAndDrop() {
+    console.log("work in progress");
+}; 
+
+function removeItem(itemNumber) {
+    const li = document.querySelectorAll('#items li')
+    console.log(li[itemNumber], "index: " + itemNumber);
+    li[itemNumber].remove();
 }
 
 
+// Event listeners ---
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-// function removeItemChild(itemNumber) {
-//     const li = document.querySelectorAll('#items li')
-//     console.log(li[itemNumber], "index: " + itemNumber);
-//     li[itemNumber].remove();
-// }
+    const item = document.querySelector('#inputItem').value;
+    addItem(item);
+});
+
+clearButton.addEventListener('click', clearAll)
+cartShopping.addEventListener('dblclick', doubleClick);
 
 
 
@@ -47,5 +66,7 @@ function clearAll() {
 
 
 
-// drag and drop feature
+
+
+
 
